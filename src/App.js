e598ed4a2e72ccb424c1665709ca8911701ed565
Joey Steigelman <joey.steigelman@gmail.com>
2022-03-29 14:26:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Course = (props) => {
+  const course = props.course
+  let exercises = []
+
+  const parts = course.parts.map((part) => {
+    exercises.push(part.exercises)
+    return <li key={part.id}>{part.name} {part.exercises}</li>
+  })
+
+  const sumExercises = exercises.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>{course.name}</h1>
+    <p>{parts}</p>
+    <p>There are {sumExercises} exercises in total</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
